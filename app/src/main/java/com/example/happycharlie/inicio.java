121931eb89e264798c5cc5c3c3f765845a1abf9a
Demby <dominubus@gmail.com>
2021-04-25 20:40:33 -0400

    Mostrar mensaje de animo en inicio

--- app/src/main/java/com/example/happycharlie/inicio.java
+++ app/src/main/java/com/example/happycharlie/inicio.java
@@ -1,6 +1,8 @@
 package com.example.happycharlie;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -14,10 +16,13 @@ import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class inicio extends AppCompatActivity {
 
-
+    private TextView tv;
     Handler mHideHandler = new Handler();
     Runnable mHideRunnable = new Runnable() {
         @Override
@@ -39,7 +44,8 @@ public class inicio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
         mHideHandler.postDelayed(mHideRunnable, 30000);
-
+        tv = (TextView)findViewById(R.id.textView2);
+        tv.setText(BuscarCodigo());
 
         ImageButton btnClose = (ImageButton) findViewById(R.id.btnClose);
         btnClose.setOnClickListener(new View.OnClickListener() {
@@ -56,5 +62,31 @@ public class inicio extends AppCompatActivity {
 
         Intent siguiente = new Intent(this, lobby.class);
         startActivity(siguiente);
+    }
+
+    //Genera numero random y lo convierte en String
+    public String generarNroRandom(){
+        Random miRandom = new Random();
+        return String.valueOf(miRandom.nextInt(10));
+    }
+
+    //Metodo para buscar codigo del mensaje en la BD con un numero random
+    public String BuscarCodigo() {
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "administracion", null, 1);
+        SQLiteDatabase BaseDeDatos = admin.getWritableDatabase();
+        String men = "";
+        String codigo = generarNroRandom();
+        Cursor fila = BaseDeDatos.rawQuery("select mensaje from mensajes where codigo =" + codigo, null);
+        if(fila.moveToFirst()){
+            men = fila.getString(0);
+            BaseDeDatos.close();
+        }
+        return men;
+    }
+
+    //Metodo para mandar mensaje al TextView
+    public void mandarMensaje(View view){
+        String mensajeAleatoreo = BuscarCodigo();
+        tv.setText(mensajeAleatoreo);
     }
 }
