183a9a88afe246023dc4f4a1a9b6bf1c553ad78e
Demby <dominubus@gmail.com>
2021-05-09 17:08:51 -0400

    Mostrar mensaje pantalla meditacion

--- app/src/main/java/com/example/happycharlie/meditacion.java
+++ app/src/main/java/com/example/happycharlie/meditacion.java
@@ -1,5 +1,6 @@
 package com.example.happycharlie;
 
+import android.graphics.Rect;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,7 +9,10 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.Gravity;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class meditacion extends AppCompatActivity {
 
@@ -17,6 +21,17 @@ public class meditacion extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meditacion);
 
+    }
+
+    public void mostrarMensaje(View v){
+        int yOffset = v.getHeight();
+        Toast t=Toast.makeText(this, "Escoge una de estas fabulosas guías", Toast.LENGTH_LONG);
+        View view = t.getView();
+
+        TextView text = (TextView)view.findViewById(android.R.id.message);
+        text.setTextAppearance(R.style.toastTextStyle);
+        t.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL, 0, -yOffset);
+        t.show();
     }
 
 }
