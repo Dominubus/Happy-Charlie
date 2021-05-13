c958452b1861f5ef5ddd3e3a0b7984d427d80c8f
Elmer <dabur12vidbu345@gmail.com>
2021-05-13 18:39:43 -0400

    Se creo pantalla Reproductor y creo metodo para pasar por referencia nro cancion a pantalla reproductor

--- app/src/main/java/com/example/happycharlie/Pantalla_Musica.java
+++ app/src/main/java/com/example/happycharlie/Pantalla_Musica.java
@@ -2,13 +2,39 @@ package com.example.happycharlie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Pantalla_Musica extends AppCompatActivity {
+
+    private String numCan = "";
+    private int aux;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla__musica);
+    }
+
+    //Metodo para enviar Numero de Cancion
+    public void RecuperarNumero(View view){
+        Intent pasarPantalla = new Intent(this, Pantalla_Reproductor.class);
+        switch (view.getId()){
+            case R.id.btn_mus_1:
+                numCan = ""+1;
+                pasarPantalla.putExtra("dato", numCan);
+                startActivity(pasarPantalla);
+                break;
+            case R.id.btn_mus_2:
+                numCan = ""+2;
+                pasarPantalla.putExtra("dato", numCan);
+                startActivity(pasarPantalla);
+                break;
+            case R.id.btn_mus_3:
+                numCan = ""+3;
+                pasarPantalla.putExtra("dato", numCan);
+                startActivity(pasarPantalla);
+        }
     }
 }
---
+++ app/src/main/java/com/example/happycharlie/Pantalla_Reproductor.java
@@ -0,0 +1,14 @@
+package com.example.happycharlie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pantalla_Reproductor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla__reproductor);
+    }
+}
