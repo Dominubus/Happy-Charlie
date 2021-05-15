1bf1fdaf6e1ebf9a71f6db88c41fee51c08a7f6b
SusanDaniela <susandanielamamanienriquez@gmail.com>
2021-05-14 21:28:39 -0400

    crear popupTemporizador

--- app/src/main/java/com/example/happycharlie/ruido.java
+++ app/src/main/java/com/example/happycharlie/ruido.java
@@ -1,5 +1,8 @@
 package com.example.happycharlie;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,13 +12,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.ImageButton;
 
 public class ruido extends AppCompatActivity {
 
+    ImageButton btnTempo;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ruido);
+
+        btnTempo = findViewById(R.id.timerIcon);
+
+        btnTempo.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ruido.this, PopupTemporizador.class));
+            }
+        });
 
     }
 
