456b129aff9f9052ac9f578b1994b86d02080815
ProElim <elimorellanae@gmail.com>
2021-05-16 11:39:31 -0400

    Margot 3: por error en el merge se creo esta nueva rama.

--- app/src/main/java/com/example/happycharlie/musica.java
+++ app/src/main/java/com/example/happycharlie/musica.java
@@ -1,170 +1,123 @@
 package com.example.happycharlie;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import static com.example.happycharlie.R.*;
 
 public class musica extends AppCompatActivity {
 
         MediaPlayer mediaPlayer;
+        String numCan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_musica);
+        setContentView(layout.activity_musica);
         mediaPlayer=null;
+        numCan= null;
     }
 
-    @SuppressLint("NonConstantResourceId")
+    public void volverLobby(View view) {
+        //Intent anterior = new Intent(this, lobby.class);
+        //startActivity(anterior);
+        finish();
+    }
+
     public void Reproduccion(View view){
         switch (view.getId()){
             case R.id.cancion1:
-                Toast.makeText(this, "Don't Stop Me Now",Toast.LENGTH_SHORT).show();
                 if(mediaPlayer==null){
-                    mediaPlayer = MediaPlayer.create(this, R.raw.a1_dont_stop_me_now);
+                    mediaPlayer = MediaPlayer.create(this, raw.a1_dont_stop_me_now);
                 }
-                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mediaPlayer) {
-                        Stop();
-                    }
-                });
                 mediaPlayer.start();
-
+                Toast.makeText(this, "Don't Stop Me Now",Toast.LENGTH_SHORT).show();
+                numCan= "0";
 
                 break;
             case R.id.cancion2:
-                Toast.makeText(this, "Dancing Queen",Toast.LENGTH_SHORT).show();
                 if(mediaPlayer==null){
-                    mediaPlayer = MediaPlayer.create(this, R.raw.a2_dancing_queen);
+                    mediaPlayer = MediaPlayer.create(this, raw.a2_dancing_queen);
                 }
-                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mediaPlayer) {
-                        Stop();
-                    }
-                });
                 mediaPlayer.start();
+                Toast.makeText(this, "Dancing Queen",Toast.LENGTH_SHORT).show();
+                numCan= "1";
                 break;
             case R.id.cancion3:
-                Toast.makeText(this, "Good Vibrations",Toast.LENGTH_SHORT).show();
                 if(mediaPlayer==null){
-                    mediaPlayer = MediaPlayer.create(this, R.raw.a3_good_vibrations);
+                    mediaPlayer = MediaPlayer.create(this, raw.a3_good_vibrations);
                 }
-                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mediaPlayer) {
-                        Stop();
-                    }
-                });
                 mediaPlayer.start();
+                Toast.makeText(this, "Good Vibrations",Toast.LENGTH_SHORT).show();
+                numCan= "2";
                 break;
             case R.id.cancion4:
-                Toast.makeText(this, "Uptown Girl",Toast.LENGTH_SHORT).show();
                 if(mediaPlayer==null){
-                    mediaPlayer = MediaPlayer.create(this, R.raw.a4_uptown_girl);
+                    mediaPlayer = MediaPlayer.create(this, raw.a4_uptown_girl);
                 }
-                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mediaPlayer) {
-                        Stop();
-                    }
-                });
                 mediaPlayer.start();
+                Toast.makeText(this, "Uptown Girl",Toast.LENGTH_SHORT).show();
+                numCan= "3";
                 break;
             case R.id.cancion5:
-                Toast.makeText(this, "Eye Of The Tiger",Toast.LENGTH_SHORT).show();
                 if(mediaPlayer==null){
-                    mediaPlayer = MediaPlayer.create(this, R.raw.a5_eye_of_the_tiger);
+                    mediaPlayer = MediaPlayer.create(this, raw.a5_eye_of_the_tiger);
                 }
-                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mediaPlayer) {
-                        Stop();
-                    }
-                });
                 mediaPlayer.start();
+                Toast.makeText(this, "Eye Of The Tiger",Toast.LENGTH_SHORT).show();
+                numCan= "4";
                 break;
             case R.id.cancion6:
-                Toast.makeText(this, "I'm a Believer",Toast.LENGTH_SHORT).show();
                 if(mediaPlayer==null){
-                    mediaPlayer = MediaPlayer.create(this, R.raw.a6_im_a_believer);
+                    mediaPlayer = MediaPlayer.create(this, raw.a6_im_a_believer);
                 }
-                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mediaPlayer) {
-                        Stop();
-                    }
-                });
                 mediaPlayer.start();
+                Toast.makeText(this, "I'm a Believer",Toast.LENGTH_SHORT).show();
+                numCan= "5";
                 break;
             case R.id.cancion7:
-                Toast.makeText(this, "Girls Just Want To Have Fun",Toast.LENGTH_SHORT).show();
                 if(mediaPlayer==null){
-                    mediaPlayer = MediaPlayer.create(this, R.raw.a7_girls_just_want_to_have_fun);
+                    mediaPlayer = MediaPlayer.create(this, raw.a7_girls_just_want_to_have_fun);
                 }
-                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mediaPlayer) {
-                        Stop();
-                    }
-                });
                 mediaPlayer.start();
+                Toast.makeText(this, "Girls Just Want To Have Fun",Toast.LENGTH_SHORT).show();
+                numCan= "6";
                 break;
             case R.id.cancion8:
-                Toast.makeText(this, "Living On A Prayer",Toast.LENGTH_SHORT).show();
                 if(mediaPlayer==null){
-                    mediaPlayer = MediaPlayer.create(this, R.raw.a8_livin_on_a_prayer);
+                    mediaPlayer = MediaPlayer.create(this, raw.a8_livin_on_a_prayer);
                 }
-                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mediaPlayer) {
-                        Stop();
-                    }
-                });
                 mediaPlayer.start();
+                Toast.makeText(this, "Living On A Prayer",Toast.LENGTH_SHORT).show();
+                numCan= "7";
                 break;
             case R.id.cancion9:
-                Toast.makeText(this, "I Will Survive",Toast.LENGTH_SHORT).show();
                 if(mediaPlayer==null){
-                    mediaPlayer = MediaPlayer.create(this, R.raw.a9_i_will_survive);
+                    mediaPlayer = MediaPlayer.create(this, raw.a9_i_will_survive);
                 }
-                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mediaPlayer) {
-                        Stop();
-                    }
-                });
+                Toast.makeText(this, "I Will Survive",Toast.LENGTH_SHORT).show();
                 mediaPlayer.start();
+                numCan= "8";
                 break;
             case R.id.cancion10:
-                Toast.makeText(this, "Walking On Sunshine",Toast.LENGTH_SHORT).show();
                 if(mediaPlayer==null){
-                    mediaPlayer = MediaPlayer.create(this, R.raw.a10_walking_on_sunshine);
+                    mediaPlayer = MediaPlayer.create(this, raw.a10_walking_on_sunshine);
                 }
-                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mediaPlayer) {
-                        Stop();
-                    }
-                });
                 mediaPlayer.start();
+                Toast.makeText(this, "Walking On Sunshine",Toast.LENGTH_SHORT).show();
+                numCan= "9";
                 break;
         }
-    }
-    private void Stop() {
-        mediaPlayer.release();
-        mediaPlayer = null;
-    }
-    @Override
-    protected void onStop(){
-        super.onStop();
-        Stop();
+        Intent pasarPantalla = new Intent(this, Pantalla_Reproductor.class);
+        pasarPantalla.putExtra ("dato",numCan);
+        startActivity(pasarPantalla);
+
     }
 }
 
