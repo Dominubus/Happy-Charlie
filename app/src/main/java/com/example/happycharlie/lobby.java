f554a91ced731fdc7e3f8b4f48514ed60dcd6461
Jhilm <jnukuravuisente28@gmail.com>
2021-06-20 19:21:21 -0400

    fondo

--- app/src/main/java/com/example/happycharlie/lobby.java
+++ app/src/main/java/com/example/happycharlie/lobby.java
@@ -1,6 +1,8 @@
 package com.example.happycharlie;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,10 +10,12 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Handler;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -30,9 +34,55 @@ public class lobby extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+    }
 
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sp = getSharedPreferences("FONDO_INICIO", MODE_PRIVATE);
+        int key = sp.getInt("key", 1);
+        ConstraintLayout layout = findViewById(R.id.lobby);
+        switch(key) {
+            case 1:
+                layout.setBackgroundResource(R.drawable.bghuellitas);
+                break;
+            case 2:
+                layout.setBackgroundResource(R.drawable.bg2);
+                break;
 
+            case 3:
+                layout.setBackgroundResource(R.drawable.bg3);
+                break;
+            default:
+                layout.setBackgroundResource(R.drawable.bghuellitas2);
+                break;
+        }
+        TextView mensaje=findViewById(R.id.textView2);
+        sp = getSharedPreferences("COLOR_MENSAJE", MODE_PRIVATE);
+        key = sp.getInt("key", 1);
+        switch(key) {
+            case 1:
+                mensaje.setTextColor(Color.parseColor("#000000"));
+                break;
+            case 2:
+                mensaje.setTextColor(Color.parseColor("#D2691E"));
+                break;
 
+            case 3:
+                mensaje.setTextColor(Color.parseColor("#0000FF"));
+                break;
+            case 4:
+                mensaje.setTextColor(Color.parseColor("#DC143C"));
+                break;
+            case 5:
+                mensaje.setTextColor(Color.parseColor("#8B008B"));
+                break;
+            case 6:
+                mensaje.setTextColor(Color.parseColor("#008000"));
+                break;
+            default:
+                mensaje.setTextColor(Color.parseColor("#000000"));
+                break;
+        }
     }
 
     public void pantallaAnimo(View view) {
