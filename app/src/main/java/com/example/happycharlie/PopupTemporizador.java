f2f8641e2700de38d603204618924a8207de3103
Elmer David Mamani Quispe <dabur12vidbu345@gmail.com>
2021-06-29 01:06:45 -0400

    Se agrego cambio de fuente en popupTemporizador

--- app/src/main/java/com/example/happycharlie/PopupTemporizador.java
+++ app/src/main/java/com/example/happycharlie/PopupTemporizador.java
@@ -2,20 +2,28 @@ package com.example.happycharlie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class PopupTemporizador extends AppCompatActivity {
     ruido ruido = new ruido();
+    private Button b1, b2, b3, b4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_popup_temporizador);
+        b1 = findViewById(R.id.botonOne);
+        b2 = findViewById(R.id.botonTwo);
+        b3 = findViewById(R.id.botonThree);
+        b4 = findViewById(R.id.botonFour);
 
         DisplayMetrics medidasVentana = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(medidasVentana);
@@ -56,5 +64,31 @@ public class PopupTemporizador extends AppCompatActivity {
         Toast.makeText(this, "Establecido en 3 horas", Toast.LENGTH_SHORT).show();
         ruido.setTimer(10800000);
         finish();
+    }
+
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sp = getSharedPreferences("FUENTE_SISTEMA", MODE_PRIVATE);
+        int key = sp.getInt("keyf", 1);
+        b1 = findViewById(R.id.botonOne);
+        b2 = findViewById(R.id.botonTwo);
+        b3 = findViewById(R.id.botonThree);
+        b4 = findViewById(R.id.botonFour);
+        switch(key) {
+            case 1:
+                Typeface nom_fuen = Typeface.createFromAsset(getAssets(), "font/merriweather_regular.ttf");
+                b1.setTypeface(nom_fuen);
+                b2.setTypeface(nom_fuen);
+                b3.setTypeface(nom_fuen);
+                b4.setTypeface(nom_fuen);
+                break;
+            case 2:
+                Typeface nom_fue = Typeface.createFromAsset(getAssets(), "font/raleway_bold.ttf");
+                b1.setTypeface(nom_fue);
+                b2.setTypeface(nom_fue);
+                b3.setTypeface(nom_fue);
+                b4.setTypeface(nom_fue);
+                break;
+        }
     }
 }
